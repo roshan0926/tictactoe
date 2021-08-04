c3491484d09cc605a863f5c6ff763310d9a5d933
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-08-04 00:07:11 -0700

    set up components and route

---
+++ tictactoe/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Grid from '../Grid/Grid'
+import Players from '../Players/Players';
+
+export default function Home() {
+  return <>
+  Home 
+  <Players />
+  <Grid />
+  </>
+}
