ab24cedef9d2b24bfe68a8960ecdfcf5594196eb
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-08-04 02:43:59 -0700

    create the basic grid and log the clicked box

--- tictactoe/src/components/Grid/Grid.jsx
+++ tictactoe/src/components/Grid/Grid.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './grid.scss';
+
 
 export default function Grid() {
+  const boxNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [box, newBox] = useState(0)
+
+  const onClickBox = (e) => {
+    const index = +e.target.getAttribute("data-key")
+    newBox(index)
+    console.log(`clicked box ${boxNumbers[index]}`)
+  }
+
+
+
   return <>
-  Grid
+  hi
+    <div className="page">
+      {boxNumbers.filter((box, index) => box === index).map((box, index) =>
+        <div className="boxes" />)}
+      <div className="grid">
+        {/* render boxes for box highligting baced on number of boxes*/}
+        {boxNumbers.map((box, index) => {
+          if (index === box) {
+            return <span className="box box-highlighted" key={index} data-key={index}></span>
+          }
+          return <span className="box" key={index} data-key={index} onClick={onClickBox}></span>
+        })}
+      </div>
+    </div>
   </>
 }
